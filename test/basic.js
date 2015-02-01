0348dd22f0cbdb185d36c40f03f46afd2bba967d
Feross Aboukhadijeh <feross@feross.org>
2015-01-31 16:07:30 -0800

    add basic sanity check tests

---
+++ test/basic.js
@@ -0,0 +1,39 @@
+var Discovery = require('../')
+var DHT = require('bittorrent-dht')
+var hat = require('hat')
+var test = require('tape')
+
+test('initialize with dht', function (t) {
+  var dht = new DHT()
+  var discovery = new Discovery({
+    peerId: hat(20),
+    port: 6000,
+    dht: dht
+  })
+  discovery.stop(function () {
+    dht.destroy(function () {
+      t.end()
+    })
+  })
+})
+
+test('initialize with default dht', function (t) {
+  var discovery = new Discovery({
+    peerId: hat(20),
+    port: 6000
+  })
+  discovery.stop(function () {
+    t.end()
+  })
+})
+
+test('initialize without dht', function (t) {
+  var discovery = new Discovery({
+    peerId: hat(20),
+    port: 6000,
+    dht: false
+  })
+  discovery.stop(function () {
+    t.end()
+  })
+})
